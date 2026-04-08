230f1da414e73437fddb7be92f8c360ddfb7134d
上帝的梦 <1597691532@qq.com>
2026-04-08 11:39:06 +0800

    1

---
+++ index.dde14.js
@@ -0,0 +1,70 @@
+System.register(["./application.be3bb.js"], function (_export, _context) {
+  "use strict";
+
+  var Application, canvas, $p, bcr, application;
+
+  function topLevelImport(url) {
+    return System["import"](url);
+  }
+
+  return {
+    setters: [function (_applicationBe3bbJs) {
+      Application = _applicationBe3bbJs.Application;
+    }],
+    execute: function () {
+      canvas = document.getElementById('GameCanvas');
+      $p = canvas.parentElement;
+      bcr = $p.getBoundingClientRect();
+      canvas.width = bcr.width;
+      canvas.height = bcr.height;
+      application = new Application();
+      topLevelImport('cc').then(function (engine) {
+        return application.init(engine);
+      }).then(function () {
+        const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+        function adjustCocosSizing() {
+          if (!isMobile && window.innerWidth >= 768) {
+              const container = document.getElementById('GameDiv');
+              const canvas = document.getElementById('GameCanvas');
+              
+              // 确保canvas正确渲染
+              if (window.cc && cc.game) {
+                  cc.game.container = container;
+                  cc.game.canvas = canvas;
+                  cc.game.frame = container;
+                  
+                  // 通知Cocos重新计算大小
+                  if (cc.view) {
+                      cc.view.setDesignResolutionSize(
+                          760,
+                          1334,
+                          cc.ResolutionPolicy.SHOW_ALL
+                      );
+                  }
+              }
+          }
+        }
+        if (!isMobile){
+          // 监听事件
+          window.addEventListener('load', adjustCocosSizing);
+          window.addEventListener('resize', adjustCocosSizing);
+          
+          // 如果使用了cc.game.onStart，在其中也调用
+          if (window.cc && cc.game) {
+            const originalOnStart = cc.game.onStart;
+            cc.game.onStart = function() {
+                if (originalOnStart) {
+                    originalOnStart.apply(this, arguments);
+                }
+                adjustCocosSizing();
+            };
+          }
+        }
+
+return application.start();
+      })["catch"](function (err) {
+        console.error(err);
+      });
+    }
+  };
+});
---
+++ run.bat
@@ -0,0 +1,1 @@
+http-server -p 9832 -c-1
